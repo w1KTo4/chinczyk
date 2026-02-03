8ce305f8c6ab87060478e8bd8932a2190eba9831
Your Name <wiktor.rutkowski@tm1.edu.pl>
2026-02-03 10:18:14 +0100

    Commit

--- src/Panel.java
+++ src/Panel.java
@@ -1,28 +1,22 @@
-import board.Board;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.*;
+import board.Board;
 import model.Piece;
 import model.PieceColor;
-
 public class Panel extends JPanel implements MouseListener {
-
     private final int ROZMIAR_POLA = 55;
     private final int ILOSC_POL = 15;
-
     private Board board;
     private int liczba = 0;
-
     public Panel() {
         addMouseListener(this);
         this.setPreferredSize(new Dimension(ROZMIAR_POLA * ILOSC_POL, ROZMIAR_POLA * ILOSC_POL));
         this.setBackground(Color.WHITE);
-
-        board = new Board(ILOSC_POL);
-        board.initializeStartPositions();
+        this.board = new Board(ILOSC_POL);
+        this.board.initializeStartPositions();
     }
-
     // ---------------------------
     // Eventy myszy
     // ---------------------------
@@ -30,56 +24,45 @@ public class Panel extends JPanel implements MouseListener {
     public void mouseClicked(MouseEvent e) {
         int kolumn = e.getX() / ROZMIAR_POLA;
         int wiersz = e.getY() / ROZMIAR_POLA;
-
         if (kolumn < 0 || kolumn >= ILOSC_POL || wiersz < 0 || wiersz >= ILOSC_POL) {
             return;
         }
-  
+
         if (wiersz == 7 && kolumn == 7) {
-            liczba = (int) (Math.random() * 6) + 1; 
+            liczba = (int) (Math.random() * 6) + 1; // 1..6
             repaint();
             return;
         }
 
         Piece p = board.getPieceAt(wiersz, kolumn);
         if (p == null) return;
-
         PieceColor c = p.getColor();
         if (c == null) return;
 
-  
         if (board.tryMoveFromBase(wiersz, kolumn)) {
             repaint();
             return;
         }
-        if (c == PieceColor.GREEN || c == PieceColor.BLUE || c == PieceColor.YELLOW || c == PieceColor.RED) {
 
-            if (c == PieceColor.RED && liczba <= 0) return;
+        if (c == PieceColor.RED && liczba <= 0) return;
+        int oldCounter = p.getCounter();
+        int nowyCounter = oldCounter + liczba;
+        if (nowyCounter >= 56) nowyCounter = 55;
+        Point cel = board.getPositionForColorAndCounter(c, nowyCounter);
 
-            int oldCounter = p.getCounter();
-            int nowyCounter = oldCounter + liczba;
-            if (nowyCounter >= 56) nowyCounter = 55;
-            Point cel = board.getPositionForColorAndCounter(c, nowyCounter);
+        boolean moved = board.attemptMove(wiersz, kolumn, cel.y, cel.x, nowyCounter);
+        if (!moved) {
 
-            if (board.isOccupied(cel.y, cel.x)) {
-                return;
-            }
-
-            board.movePiece(wiersz, kolumn, cel.y, cel.x, nowyCounter);
-            liczba = 0;
-            repaint();
             return;
         }
-    }
 
-    // ---------------------------
-    // Puste implementacje MouseListener
-    // ---------------------------
+        liczba = 0;
+        repaint();
+    }
     @Override public void mousePressed(MouseEvent e) {}
     @Override public void mouseReleased(MouseEvent e) {}
     @Override public void mouseEntered(MouseEvent e) {}
     @Override public void mouseExited(MouseEvent e) {}
-
     // ---------------------------
     // Rysowanie
     // ---------------------------
@@ -93,26 +76,24 @@ public class Panel extends JPanel implements MouseListener {
             for (int k = 0; k < ILOSC_POL; k++) {
                 Piece piece = board.getPieceAt(w, k);
                 if (piece != null) {
-                    rysujPionek(g, k, w, piece.getAWTColor(), ROZMIAR_POLA);
+                    rysujPionek(g, k, w, piece.getAWTColor());
                 }
             }
         }
 
         if (liczba > 0) {
             rysujLiczba(
-                g,
-                7 * ROZMIAR_POLA + ROZMIAR_POLA / 2,
-                7 * ROZMIAR_POLA + ROZMIAR_POLA / 2,
-                liczba
+                    g,
+                    7 * ROZMIAR_POLA + ROZMIAR_POLA / 2,
+                    7 * ROZMIAR_POLA + ROZMIAR_POLA / 2,
+                    liczba
             );
         }
     }
-
-    private void rysujPionek(Graphics g, int x, int y, Color kolor, int size) {
+    private void rysujPionek(Graphics g, int x, int y, Color kolor) {
         g.setColor(kolor);
-        g.fillOval(x * size + 5, y * size + 5, size - 10, size - 10);
+        g.fillOval(x * ROZMIAR_POLA + 5, y * ROZMIAR_POLA + 5, ROZMIAR_POLA - 10, ROZMIAR_POLA - 10);
     }
-
     private void rysujLiczba(Graphics g, int x, int y, int liczba) {
         g.setColor(Color.BLACK);
         g.setFont(new Font("Arial", Font.BOLD, 20));
@@ -123,13 +104,11 @@ public class Panel extends JPanel implements MouseListener {
         g.drawString(tekst, tekstX, tekstY);
     }
 
-public void restartGame() {
-
-    if (this.board != null) {
-        this.board.initializeStartPositions();
+    public void restartGame() {
+        if (this.board != null) {
+            board.initializeStartPositions();
+        }
+        this.liczba = 0;
+        repaint();
     }
-
-    this.liczba = 0;
-    repaint();
-}
 }
--- src/board/Board.java
+++ src/board/Board.java
@@ -7,84 +7,64 @@ import route.GreenRouteCalculator;
 import route.RedRouteCalculator;
 import route.RouteCalculator;
 import route.YellowRouteCalculator;
-
 public class Board {
     private final int size;
     private final Color[][] mapa;
     private final Piece[][] pieces;
-
     private final RouteCalculator redRoute = new RedRouteCalculator();
     private final RouteCalculator greenRoute = new GreenRouteCalculator();
     private final RouteCalculator blueRoute = new BlueRouteCalculator();
     private final RouteCalculator yellowRoute = new YellowRouteCalculator();
-
     public Board(int size) {
         this.size = size;
         mapa = new Color[size][size];
         pieces = new Piece[size][size];
     }
-
     public void initializeStartPositions() {
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-              pieces[i][j] = null; 
-            
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                pieces[i][j] = null;
+            }
+        }
+        int[][] pozycje = {
+                {2, 2}, {3, 2}, {2, 3}, {3, 3},       // czerwone
+                {11, 2}, {11, 3}, {12, 2}, {12, 3},   // zielone
+                {2, 11}, {3, 11}, {2, 12}, {3, 12},   // niebieskie
+                {11, 11}, {12, 11}, {11, 12}, {12, 12} // żółte
+        };
+        for (int counter = 0; counter < pozycje.length; counter++) {
+            int col = pozycje[counter][0];
+            int row = pozycje[counter][1];
+            PieceColor color;
+            if (counter < 4) color = PieceColor.RED;
+            else if (counter < 8) color = PieceColor.GREEN;
+            else if (counter < 12) color = PieceColor.BLUE;
+            else color = PieceColor.YELLOW;
+            Piece piece = new Piece(color, 0);
+            setPieceAt(row, col, piece);
         }
     }
-
-
-    int[][] pozycje = {
-        {2, 2}, {3, 2}, {2, 3}, {3, 3},       // czerwone
-        {11, 2}, {11, 3}, {12, 2}, {12, 3},   // zielone
-        {2, 11}, {3, 11}, {2, 12}, {3, 12},   // niebieskie
-        {11, 11}, {12, 11}, {11, 12}, {12, 12} // żółte
-    };
-
-    for (int counter = 0; counter < pozycje.length; counter++) {
-        int row = pozycje[counter][1];
-        int col = pozycje[counter][0];
-        PieceColor color;
-
-        if (counter < 4) color = PieceColor.RED;
-        else if (counter < 8) color = PieceColor.GREEN;
-        else if (counter < 12) color = PieceColor.BLUE;
-        else color = PieceColor.YELLOW;
-
-        Piece piece = new Piece(color, 0); 
-        setPieceAt(row, col, piece);
-    }
-}
-
-
-
     public Piece getPieceAt(int row, int col) {
         if (valid(row, col)) return pieces[row][col];
         return null;
     }
-
     public boolean isOccupied(int row, int col) {
         if (!valid(row, col)) return true;
         return pieces[row][col] != null;
     }
-
     public void setPieceAt(int row, int col, Piece p) {
         if (valid(row, col)) pieces[row][col] = p;
     }
-
     public void removePieceAt(int row, int col) {
         if (valid(row, col)) pieces[row][col] = null;
     }
-
     private boolean valid(int r, int c) {
         return r >= 0 && r < size && c >= 0 && c < size;
     }
-
     public boolean tryMoveFromBase(int row, int col) {
         Piece p = getPieceAt(row, col);
         if (p == null) return false;
         PieceColor c = p.getColor();
-
         // czerwony
         if (c == PieceColor.RED && row >= 1 && row <= 4 && col >= 1 && col <= 4) {
             int sr = 0, sc = 8;
@@ -95,7 +75,6 @@ public class Board {
                 return true;
             } else return false;
         }
-
         // zielony
         if (c == PieceColor.GREEN && row >= 1 && row <= 4 && col >= 10 && col <= 13) {
             int sr = 8, sc = 14;
@@ -106,7 +85,6 @@ public class Board {
                 return true;
             } else return false;
         }
-
         // niebieski
         if (c == PieceColor.BLUE && row >= 10 && row <= 13 && col >= 1 && col <= 4) {
             int sr = 6, sc = 0;
@@ -117,7 +95,6 @@ public class Board {
                 return true;
             } else return false;
         }
-
         // zolty
         if (c == PieceColor.YELLOW && row >= 10 && row <= 13 && col >= 10 && col <= 13) {
             int sr = 14, sc = 6;
@@ -128,11 +105,8 @@ public class Board {
                 return true;
             } else return false;
         }
-
         return false;
     }
-
-
     public void movePiece(int fromRow, int fromCol, int toRow, int toCol, int newCounter) {
         Piece p = getPieceAt(fromRow, fromCol);
         if (p == null) return;
@@ -141,7 +115,54 @@ public class Board {
         setPieceAt(toRow, toCol, p);
     }
 
-
+    public boolean attemptMove(int fromRow, int fromCol, int toRow, int toCol, int newCounter) {
+        Piece moving = getPieceAt(fromRow, fromCol);
+        if (moving == null) return false;
+        if (!valid(toRow, toCol)) return false;
+        Piece target = getPieceAt(toRow, toCol);
+        if (target != null) {
+            if (target.getColor() == moving.getColor()) {
+                return false;
+            }
+            removePieceAt(toRow, toCol);
+            sendPieceToBase(target);
+        }
+        removePieceAt(fromRow, fromCol);
+        moving.setCounter(newCounter);
+        setPieceAt(toRow, toCol, moving);
+        return true;
+    }
+    private void sendPieceToBase(Piece piece) {
+        piece.setCounter(0);
+        Point base = getFreeBasePosition(piece.getColor());
+        setPieceAt(base.y, base.x, piece);
+    }
+    private Point getFreeBasePosition(PieceColor color) {
+        int[][] basePositions;
+        switch (color) {
+            case RED:
+                basePositions = new int[][] { {2,2}, {3,2}, {2,3}, {3,3} };
+                break;
+            case GREEN:
+                basePositions = new int[][] { {11,2}, {11,3}, {12,2}, {12,3} };
+                break;
+            case BLUE:
+                basePositions = new int[][] { {2,11}, {3,11}, {2,12}, {3,12} };
+                break;
+            case YELLOW:
+            default:
+                basePositions = new int[][] { {11,11}, {12,11}, {11,12}, {12,12} };
+                break;
+        }
+        for (int[] p : basePositions) {
+            int col = p[0];
+            int row = p[1];
+            if (!isOccupied(row, col)) {
+                return new Point(col, row);
+            }
+        }
+        return new Point(basePositions[0][0], basePositions[0][1]);
+    }
     public Point getPositionForColorAndCounter(PieceColor color, int counter) {
         Point p;
         switch (color) {
@@ -160,15 +181,11 @@ public class Board {
             default:
                 p = redRoute.getPosition(counter);
         }
-
         int k = Math.max(0, Math.min(size - 1, p.x));
         int w = Math.max(0, Math.min(size - 1, p.y));
         return new Point(k, w);
     }
-
-
     public void drawBoard(Graphics g, int ROZMIAR_POLA) {
-        // 0) wyczyść mapę
         for (int w = 0; w < size; w++) {
             for (int k = 0; k < size; k++) {
                 mapa[w][k] = null;
@@ -194,13 +211,11 @@ public class Board {
             mapa[i][6] = Color.GRAY;
             mapa[i][8] = Color.GRAY;
         }
-
         mapa[0][7] = Color.GRAY;
         mapa[7][0] = Color.GRAY;
         mapa[7][14] = Color.GRAY;
         mapa[14][7] = Color.GRAY;
-        mapa[7][7] = Color.WHITE; 
-
+        mapa[7][7] = Color.WHITE;
 
         for (int w = 0; w < size; w++) {
             for (int k = 0; k < size; k++) {
@@ -218,19 +233,14 @@ public class Board {
             }
         }
 
-
         g.setColor(Color.RED);
-        g.fillRect(ROZMIAR_POLA, ROZMIAR_POLA, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4);    
-
+        g.fillRect(ROZMIAR_POLA, ROZMIAR_POLA, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4);         // lewy górny
         g.setColor(Color.GREEN);
-        g.fillRect(ROZMIAR_POLA * 10, ROZMIAR_POLA, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4); 
-
+        g.fillRect(ROZMIAR_POLA * 10, ROZMIAR_POLA, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4);    // prawy górny
         g.setColor(Color.YELLOW);
-        g.fillRect(ROZMIAR_POLA * 10, ROZMIAR_POLA * 10, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4); 
-
+        g.fillRect(ROZMIAR_POLA * 10, ROZMIAR_POLA * 10, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4); // prawy dolny
         g.setColor(Color.BLUE);
-        g.fillRect(ROZMIAR_POLA, ROZMIAR_POLA * 10, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4);   
-        
+        g.fillRect(ROZMIAR_POLA, ROZMIAR_POLA * 10, ROZMIAR_POLA * 4, ROZMIAR_POLA * 4);    // lewy dolny
         g.setColor(Color.BLACK);
         for (int w = 0; w < size; w++) {
             for (int k = 0; k < size; k++) {
